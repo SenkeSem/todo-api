bdb9febb5247e981ff345c9a36a80db6a7ac7624
SenkeSem <sen9morf@gmail.com>
2024-07-10 12:17:31 +0300

    added MONGO_URL

--- src/contsants/index.ts
+++ src/contsants/index.ts
@@ -1,4 +1,5 @@
-export const COOKIE_AUTH = 'TODO-APP-AUTH'
+export const COOKIE_AUTH = 'TODO-APP-AUTH';
 // TODO: please write me for getting this url
-export const MONGO_URL = ''
-export const SECRET = 'GENJOB_REST_API'
+export const MONGO_URL =
+  'mongodb+srv://genjob6:genjob6@cluster0.nzwuqx1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+export const SECRET = 'GENJOB_REST_API';
